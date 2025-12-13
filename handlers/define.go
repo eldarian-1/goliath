df4439ccacbf6a64e223b34c0343b166e6a5bc6b
Eldarian <ciborg.brain@gmail.com>
2025-12-13 17:35:57 +0300

    add interfaces for handler

---
+++ handlers/define.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"goliath/handlers/api/v1"
+)
+
+var handlers []Handler
+
+func init() {
+	handlers = []Handler{
+		v1.Echo{},
+	}
+}
+
+func Define() {
+	e := echo.New()
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	for _, h := range handlers {
+		e.Add(h.GetMethod(), h.GetPath(), h.DoHandle)
+	}
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
--- server.go
+++ server.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-
 	"goliath/handlers"
+	"goliath/migrations"
 )
 
 func main() {
-	e := echo.New()
-
-	e.Use(middleware.RequestLogger())
-	e.Use(middleware.Recover())
-
-	handlers.Bind(e)
-
-	e.Logger.Fatal(e.Start(":8080"))
+	migrations.Migrate()
+	handlers.Define()
 }
