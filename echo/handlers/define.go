abec633cded703da71dd2a52fc320e18bd74dc92
Eldarian <ciborg.brain@gmail.com>
2025-12-21 23:51:56 +0300

    Add middleware for internal_server_error

--- echo/handlers/define.go
+++ echo/handlers/define.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
 	"goliath/handlers/api/v1"
 	"goliath/handlers/api/v1/users"
+	"goliath/types/api"
 )
 
 var handlers []Handler
@@ -19,11 +22,26 @@ func init() {
 	}
 }
 
+func MyMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		err := next(c)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, api.Error{
+				Code:    "internal_server_error",
+				Message: err.Error(),
+			})
+		}
+
+		return nil
+	}
+}
+
 func Define() {
 	e := echo.New()
 
 	e.Use(middleware.RequestLogger())
 	e.Use(middleware.Recover())
+	e.Use(MyMiddleware)
 
 	for _, h := range handlers {
 		e.Add(h.GetMethod(), h.GetPath(), h.DoHandle)
